50914bdff3cb27f759924ede3b199d2c1f94932f
rodrigoricky <rickyrodrigo.ph@gmail.com>
2023-11-23 05:14:00 +0800

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
 "use client";
-import Card from "@/components/home/card";
-import { DEPLOY_URL } from "@/lib/constants";
-import { Github, Twitter } from "@/components/shared/icons";
-import WebVitals from "@/components/home/web-vitals";
-import ComponentGrid from "@/components/home/component-grid";
-import Image from "next/image";
-import { nFormatter } from "@/lib/utils";
-import { useSignInModal } from "../components/layout/sign-in-modal";
+import { useRef, useState, useEffect} from "react";
+import { useDemoModal } from "@/components/home/demo-modal";
+import Webcam from "react-webcam";
 
-export default async function Home() {
-  const { SignInModal, setShowSignInModal } = useSignInModal();
+export default function Home() {
+  const { DemoModal, setShowDemoModal } = useDemoModal();
+  const permissionName = "camera" as PermissionName;
+  useEffect(() => {
+    
+    navigator.permissions.query({name: permissionName }).then(function(permissionStatus) {
+      if (permissionStatus.state == 'denied' ) {
+        setShowDemoModal(true)
+        console.log('Permission to use camera is denied');
+      }
+    });
+  }, []);
 
   return (
     <>
+      <DemoModal />
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
         <a
           href="https://log.rky.me"
@@ -36,70 +42,39 @@ export default async function Home() {
         >
           Developing a Webcam-Driven AI for Translating Filipino Sign Language to Text
         </p>
-        <div
-          className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0 font-medium"
-          style={{ animationDelay: "0.3s", animationFillMode: "forwards" }}
-        >
-          <a
-            className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-blue-500 bg-blue-500 px-5 py-2 text-sm text-black transition-colors hover:bg-white hover:text-black"
-            href={'https://fsl.rky.me'}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-           
-            <p onClick={() => setShowSignInModal(true)}>Try Sign Language AI</p>
+        
+      </div>
+
+      <div className="z-10 w-full max-w-xl px-6 xl:px-0 mt-14">
+        
+        <Webcam 
+            className="mx-auto mt-2 flex animate-fade-up items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white px-4 py-2 transition-all duration-75 hover:scale-105" 
+            disablePictureInPicture={true}
+            audio={false}
+            mirrored={false}
+        />
+        <div className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0 font-medium" style={{ animationFillMode: "forwards" }} >
+          <a className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-blue-500 bg-blue-500 px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black" > 
+            <p>Predicted Letters</p>
           </a>
         </div>
-      </div>
-      <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-10 px-5 md:grid-cols-4 xl:px-0">
-        {features.map(({ title, description, demo, large }) => (
-          <Card
-            key={title}
-            title={title}
-            description={description}
-            demo={
-              title === "Beautiful, reusable components" ? (
-                <ComponentGrid />
-              ) : (
-                demo
-              )
-            }
-            large={large}
-          />
-        ))}
+
+        <a className="mx-auto mt-2 flex animate-fade-up max-w-fit items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white px-4 py-2 transition-all duration-75 hover:scale-105">
+          <p className="font-medium text-gray-600">P</p>
+        </a>
+
+        <div className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0 font-medium mt-12" style={{ animationFillMode: "forwards" }} >
+          <a className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-emerald-500 bg-emerald-500 px-6 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black" > 
+            <p>Predicted Words</p>
+          </a>
+        </div>
+
+        <a className="mx-auto mt-2 flex animate-fade-up max-w-fit items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white px-10 py-3 transition-all duration-75 hover:scale-105" >
+          <p className="font-bold text-gray-600">Lorem Ipsum; Text Here</p>
+        </a>
+
+       {/* {features.map(({ title, description, demo, large }) => ( <Card key={title} title={title} description={description} demo={ title === "Beautiful, reusable components" ? ( <ComponentGrid /> ) : (demo)} large={large} /> ))} */}
       </div>
     </>
   );
 }
-
-const features = [
-  {
-    title: "Beautiful, reusable components",
-    description:
-      "Pre-built beautiful, a11y-first components, powered by [Tailwind CSS](https://tailwindcss.com/), [Radix UI](https://www.radix-ui.com/), and [Framer Motion](https://framer.com/motion)",
-    large: true,
-  },
-  {
-    title: "Performance first",
-    description:
-      "Built on [Next.js](https://nextjs.org/) primitives like `@next/font` and `next/image` for stellar performance.",
-    demo: <WebVitals />,
-    large: false,
-  },
-  {
-    title: "One-click Deploy",
-    description:
-      "Jumpstart your next project by deploying Precedent to [Vercel](https://vercel.com/) in one click.",
-    demo: (
-      <a href={DEPLOY_URL}>
-        <Image
-          src="https://vercel.com/button"
-          alt="Deploy with Vercel"
-          width={120}
-          height={30}
-          unoptimized
-        />
-      </a>
-    ),
-  }
-];
